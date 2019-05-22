e3d66930a8020b35853be81164db63a96f12890e
Justine Linscott <justinelinscott@unomaha.edu>
2019-05-22 14:18:18 -0500

    add address

--- PizzaHunter/ViewController.swift
+++ PizzaHunter/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MapKit
 
+
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
@@ -92,6 +93,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         
         
     }
+    func getAdressName(coords: CLLocation) -> String {
+        
+        var addressStringFinal : String!
+        
+        CLGeocoder().reverseGeocodeLocation(coords) { (placemark, error) in
+            if error != nil {
+                print("Hay un error")
+            } else {
+                
+                let place = placemark! as [CLPlacemark]
+                if place.count > 0 {
+                    let place = placemark![0]
+                    var addressString : String = ""
+                    if place.thoroughfare != nil {
+                        addressString = addressString + place.thoroughfare! + ", "
+                    }
+                    if place.subThoroughfare != nil {
+                        addressString = addressString + place.subThoroughfare! + "\n"
+                    }
+                    if place.locality != nil {
+                        addressString = addressString + place.locality! + " - "
+                    }
+                    if place.postalCode != nil {
+                        addressString = addressString + place.postalCode! + "\n"
+                    }
+                    if place.subAdministrativeArea != nil {
+                        addressString = addressString + place.subAdministrativeArea! + " - "
+                    }
+                    if place.country != nil {
+                        addressString = addressString + place.country!
+                    }
+                    
+                    addressStringFinal = addressString
+                }
+            }
+        }
+        return addressStringFinal
+    }
+    
     
 }
 
