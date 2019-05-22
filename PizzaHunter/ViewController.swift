5ac05274d60dee0f8aca717307eda78d113b8b21
Justine Linscott <justinelinscott@unomaha.edu>
2019-05-22 13:20:05 -0500

    add map and pizza icon

--- PizzaHunter/ViewController.swift
+++ PizzaHunter/ViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    var currentLocation : CLLocation!
+    var pizzaPlaces : [MKMapItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        mapView.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        locationManager.startUpdatingLocation()
+        }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations.first
     }
-
-
+    
+    @IBAction func whenZoomPressed(_ sender: UIBarButtonItem) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+    @IBAction func whenSearchPressed(_ sender: UIBarButtonItem) {
+        //sets up the actual request to search
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Pizza"
+        
+        //sets up the region for the search
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let center = currentLocation.coordinate
+        let region = MKCoordinateRegion(center: center, span: span)
+        request.region = region
+        
+        //creates and starts the search
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            guard let response = response else {return}
+            //if there are parks in the region they are added to the array
+            for mapItem in response.mapItems{
+                self.pizzaPlaces.append(mapItem)
+                //adds a marker where the lockation is
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        var pin = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        pin.image = UIImage(named: "Image")
+        //pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        pin.canShowCallout = true
+        let button = UIButton(type: .detailDisclosure)
+        pin.rightCalloutAccessoryView = button
+        
+        if annotation.isEqual(mapView.userLocation) {
+            return nil
+        } else {
+            return pin
+        }
+    }
 }
 
