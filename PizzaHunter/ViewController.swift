ca3a37753dd5a1f291b3cb902e21e5d90e5f7fbc
kbrown1504 <keegqbrown@gmail.com>
2019-05-23 11:50:03 -0500

    added route line

--- PizzaHunter/ViewController.swift
+++ PizzaHunter/ViewController.swift
@@ -68,6 +68,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     }
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        
         var pin = MKAnnotationView(annotation: annotation, reuseIdentifier: nil)
         pin.image = UIImage(named: "pizzaIcon")
         //pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
@@ -75,10 +77,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         let button = UIButton(type: .detailDisclosure)
         pin.rightCalloutAccessoryView = button
         
+        
         if annotation.isEqual(mapView.userLocation) {
             return nil
         } else {
             return pin
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        for overlay in self.mapView.overlays {
+            self.mapView.removeOverlay(overlay)
+        }
+        for pizzaPlace in pizzaPlaces {
+            if pizzaPlace.placemark.coordinate.longitude == view.annotation?.coordinate.longitude && pizzaPlace.placemark.coordinate.latitude == view.annotation?.coordinate.latitude{
+                getRouteLine(mapItem: pizzaPlace)
+            }
         }
     }
     
@@ -119,9 +133,63 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
     //makes the alert controller to display the name of the place and the address. Title has to be explicitly called as a parameter beacuse annotation won't exist in the scope of the get address fuction we will call it in.
     func showAlertController(title: String!, address: String!) {
         let alertController = UIAlertController(title: title, message: address, preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        let alertAction = UIAlertAction(title: "Cancel", style: .cancel){ (UIAlertAction) in
+            print ("Cancel Triggered")
+            }
+        let getDirectionsAction = UIAlertAction(title: "Directions", style: .default) { (UIAlertAction) in
+            print ("Directions Triggered")
+            //figure out how to get written direction
+        }
+        alertController.addAction(getDirectionsAction)
         alertController.addAction(alertAction)
         self.present(alertController, animated: true)
+        
+    }
+    
+    func getWrittenDirections (location: MKMapItem){
+        
+        let directionsRequest = MKDirections.Request()
+        directionsRequest.source = MKMapItem.forCurrentLocation()
+        directionsRequest.destination = location
+        let directions = MKDirections(request: directionsRequest)
+        directions.calculate { (response: MKDirections.Response?, error: Error?) in
+            let routes = response?.routes
+            let route = routes?.first
+            var x = 1
+            var directionString = ""
+            for step in route!.steps{
+                if step.instructions != ""{
+                    directionString.append("\(x): \(step.instructions)\n")
+                    x += 1
+                }
+            }
+            print (directionString)
+        }
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let renderer = MKPolylineRenderer(polyline: overlay as! MKPolyline)
+        renderer.strokeColor = .blue
+        renderer.alpha = 0.5
+        return renderer
+        
+    }
+    
+    func getRouteLine (mapItem: MKMapItem){
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem.forCurrentLocation()
+        request.destination = mapItem
+        request.transportType = .automobile
+        let directions = MKDirections(request: request)
+        directions.calculate { (response: MKDirections.Response?, error: Error?) in
+            guard let response = response else {return}
+            for route in response.routes{
+                self.mapView.addOverlay(route.polyline)
+            }
+        }
         
     }
     
